e479a5d1bf5d638d7551fea5ed2706c3b9d3a828
Sean Wang <seanwang@Seans-MacBook-Air.local>
2024-05-27 16:08:00 +1000

    allow_unsafe_werkzeug=True

--- app.py
+++ app.py
@@ -269,4 +269,4 @@ def handle_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000)
+    socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
